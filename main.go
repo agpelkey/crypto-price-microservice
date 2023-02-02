0e8079d7b5414fd5be4c33780b53c13e9e3b5b27
Alex <apelkey94@gmail.com>
2023-02-02 11:42:44 -0800

    add client

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+
+	"github.com/agpelkey/microserv-prac/client"
 )
 
 func main() {
+	client := client.New("http://localhost:3000/")
+
+	price, err := client.FetchPrice(context.Background(), "ET")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", price)
 	listenAddr := flag.String("listenaddr", ":3000", "listen address the service is running on")
 	flag.Parse()
 
 	svc := NewLoggingService(NewMetricService(&priceFetcher{}))
 
 	server := NewJSONAPIServer(*listenAddr, svc)
-	server.Run() 
+	server.Run()
 }
